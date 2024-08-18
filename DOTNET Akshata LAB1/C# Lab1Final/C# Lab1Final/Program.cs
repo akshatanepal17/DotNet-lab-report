c5fc379c2ee0638ca4dfc082884839173088da1e
Akshata Nepal <nepal030akshata71@gmail.com>
2024-08-18 18:46:53 +0545

    Add files via upload

---
+++ DOTNET Akshata LAB1/C# Lab1Final/C# Lab1Final/Program.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace C__Lab1Final
+//{
+//    using System;
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Console.Write("Enter your name: ");
+
+//            string userName = Console.ReadLine();
+
+//            Console.WriteLine($"Welcome {userName}, to dotnet technology");
+
+//            Console.WriteLine("Press any key to exit...");
+//            Console.ReadKey();
+//        }
+//    }
+
+//}
+
